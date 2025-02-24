51782a98062869ee5beee60bb7f050b8e38ba5c4
Patharanan Ngamprasertsri <s6406021622052@email.kmutnb.ac.th>
2025-02-24 20:45:53 +0700

    1

---
+++ Test1.py
@@ -0,0 +1,8 @@
+text = "Hello world"
+
+print()
+print("String is alpha =", text.isalpha())
+print("String is numeric =", text.isnumeric())
+print("String is digit =", text.isdigit())
+print("String is lower =", text.islower())
+print("String is space =", text.isspace())
