23dcd1f83abdfc6678bc419b05277df0fbdf4286
Patharanan Ngamprasertsri <s6406021622052@email.kmutnb.ac.th>
2025-02-26 16:40:24 +0700

    1

---
+++ Chapter6/1.2_,Double Quote.py
@@ -0,0 +1,13 @@
+# ใช้ Triple Single Quote
+multi_line1 = '''This is 
+a multi-line 
+string.'''
+
+# ใช้ Triple Double Quote
+multi_line2 = """This is also 
+a multi-line 
+string."""
+
+print(multi_line1)
+print("-----------")
+print(multi_line2)
