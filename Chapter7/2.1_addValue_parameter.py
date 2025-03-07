27fd66df9b6ae5ce5be2d641ce117b0d1e673251
Patharanan Ngamprasertsri <s6406021622052@email.kmutnb.ac.th>
2025-03-07 14:55:52 +0700

    2

---
+++ Chapter7/2.1_addValue_parameter.py
@@ -0,0 +1,5 @@
+def add_value(a, b):
+    return a + b  # ส่งผลรวมของ a และ b กลับไป
+
+result = add_value(5, 3)
+print(result)  # แสดงค่า 8
---
+++ Chapter7/2_addValue.py
@@ -0,0 +1,6 @@
+def add_value():
+    return 20 + 30  # คำนวณผลรวมของ 20 และ 30 แล้วส่งค่ากลับ
+
+# เรียกใช้ฟังก์ชัน
+result = add_value()
+print(result)  # แสดงค่า 50
